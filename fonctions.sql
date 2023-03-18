250184b946fd543ddf07320d0f3f139d11892651
Maeckel <114480346+Maeckel@users.noreply.github.com>
2023-03-18 18:25:41 +0100

    C2
    C2

--- fonctions.sql
+++ fonctions.sql
@@ -51,6 +51,104 @@ Create or Replace Function getNomJour( dateEntree date ) Returns varchar(10) as 
 	END ;
 $$ LANGUAGE plpgsql ;
 
+Create or Replace Function nb_occurrences_1( caractere varchar(1) , chaine varchar(100) , indiceDebut integer , indiceFin integer ) Returns varchar(100) as $$
+	DECLARE
+		resultat integer;
+		i varchar(1);
+		erreur varchar(100);
+		occurrences varchar(100);
+		x integer;
+	BEGIN
+		resultat = 0;
+		x = indiceDebut;
+		WHILE x != indiceFin + 1 LOOP
+			i = substr(chaine, x , 1);
+			x = x + 1;
+			IF caractere = i THEN
+				resultat = resultat + 1;
+			END IF;
+		END LOOP;
+		CASE
+			WHEN indiceDebut < 1 or indiceDebut > length(chaine) THEN
+				erreur = 'INTERVALLE NON RESPECTÉ POUR INDICE DE DEPART';
+				return erreur;
+			WHEN indiceFin < 1 or indiceFin > length(chaine) THEN
+				erreur = 'INTERVALLE NON RESPECTÉ POUR INDICE DE FIN';
+				return erreur;
+			ELSE
+			RAISE INFO 'Nombre d occurrence trouvé dans la chaine :' ;
+			occurrences = '' || resultat || '';
+			return occurrences;
+		END CASE;
+	END ;
+$$ LANGUAGE plpgsql ;
+
+Create or Replace Function nb_occurrences_2( caractere varchar(1) , chaine varchar(100) , indiceDebut integer , indiceFin integer ) Returns varchar(100) as $$
+	DECLARE
+		resultat integer;
+		i varchar(1);
+		erreur varchar(100);
+		occurrences varchar(100);
+		x integer;
+	BEGIN
+		resultat = 0;
+		x = indiceDebut;
+		LOOP	
+		i = substr(chaine, x , 1);
+		x = x + 1;
+		IF caractere = i THEN
+			resultat = resultat + 1;
+		END IF;
+		IF x = indiceFin + 1 THEN
+			EXIT;
+		END IF;
+		END LOOP;
+		CASE
+			WHEN indiceDebut < 1 or indiceDebut > length(chaine) THEN
+				erreur = 'INTERVALLE NON RESPECTÉ POUR INDICE DE DEPART';
+				return erreur;
+			WHEN indiceFin < 1 or indiceFin > length(chaine) THEN
+				erreur = 'INTERVALLE NON RESPECTÉ POUR INDICE DE FIN';
+				return erreur;
+			ELSE
+			RAISE INFO 'Nombre d occurrence trouvé dans la chaine :' ;
+			occurrences = '' || resultat || '';
+			return occurrences;
+		END CASE;
+	END ;
+$$ LANGUAGE plpgsql ;
+
+Create or Replace Function nb_occurrences_3( caractere varchar(1) , chaine varchar(100) , indiceDebut integer , indiceFin integer ) Returns varchar(100) as $$
+	DECLARE
+		resultat integer;
+		i varchar(1);
+		erreur varchar(100);
+		occurrences varchar(100);
+		x integer;
+	BEGIN
+		resultat = 0;
+		FOR x in indiceDebut..indiceFin LOOP
+			i = substr(chaine, x , 1);
+			IF caractere = i THEN
+				resultat = resultat + 1;
+			END IF;
+		END LOOP;
+		CASE
+			WHEN indiceDebut < 1 or indiceDebut > length(chaine) THEN
+				erreur = 'INTERVALLE NON RESPECTÉ POUR INDICE DE DEPART';
+				return erreur;
+			WHEN indiceFin < 1 or indiceFin > length(chaine) THEN
+				erreur = 'INTERVALLE NON RESPECTÉ POUR INDICE DE FIN';
+				return erreur;
+			ELSE
+			RAISE INFO 'Nombre d occurrence trouvé dans la chaine :' ;
+			occurrences = '' || resultat || '';
+			return occurrences;
+		END CASE;
+	END ;
+$$ LANGUAGE plpgsql ;
+		
+
 
 		
 		
